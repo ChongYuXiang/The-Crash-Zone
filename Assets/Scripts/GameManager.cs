ec7c58aaed6204c8b7454f042b8b227ce8443163
ChongYuXiang <s10257151@connect.np.edu.sg>
2025-07-29 11:37:42 +0800

    Car Wrap Selection
    -Tidied up selection menu
    -Added code to switch car wraps using arrays for materials and renderers
    -Added game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public string gameMode;
+    public string playerCount;
+
+    public string p1Car;
+    public string p2Car;
+
+    public int p1Wrap;
+    public int p2Wrap;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SelectGameMode(string mode, string players)
+    {
+        gameMode = mode;
+        playerCount = players;
+    }
+
+    public void SelectCar(int player, string carName)
+    {
+        if (player == 1)
+        {
+            p1Car = carName;
+        }
+        if (player == 2)
+        {
+            p2Car = carName;
+        }
+    }
+
+    public void SelectWrap(int player, int wrapIndex)
+    {
+        if (player == 1)
+        {
+            p1Wrap = wrapIndex;
+        }
+        if (player == 2)
+        {
+            p2Wrap = wrapIndex;
+        }
+    }
+}
