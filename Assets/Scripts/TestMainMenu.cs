ea3672e91648dc9036364b730685f51ae580bbb5
Coleman-Lim <S10256996@connect.np.edu.sg>
2025-07-31 23:05:53 +0800

    transition

---
+++ Assets/Scripts/TestMainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestMainMenu : MonoBehaviour
+{
+    [SerializeField] private SceneController sceneController;
+    [SerializeField] private string sceneToLoad = "transition scene 2";
+
+    public void Play()
+    {
+        sceneController.LoadScene(sceneToLoad);
+    }
+
+    
+}
