db534d8f19a3e26701a34311e3d9f65dfd6a8665
jacksiayh <s10257192@connect.np.edu.sg>
2025-08-03 06:47:59 +0800

    Initial firebase code

---
+++ Assets/Scripts/Firebase.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Firebase : MonoBehaviour
+{
+    public string firebaseURL = "https://capstone-70c48-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+    [System.Serializable]
+    public class ScoreData
+    {
+        public string username;
+        public float time;
+        public long timestamp;
+
+        public ScoreData(string username, float time)
+        {
+            this.username = username;
+            this.time = time;
+            this.timestamp = System.DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        }
+    }
+
+    public void SubmitScore(string username, float timeTaken, string gameMode, string arenaName)
+    {
+        ScoreData data = new ScoreData(username, timeTaken);
+        string path = $"{gameMode}/{arenaName}";
+        StartCoroutine(PostScore(data, path));
+    }
+
+    private IEnumerator PostScore(ScoreData scoreData, string path)
+    {
+        string json = JsonUtility.ToJson(scoreData);
+        string fullPath = $"{firebaseURL}leaderboard/{path}.json";
+
+        using (UnityWebRequest www = UnityWebRequest.Put(fullPath, json))
+        {
+            www.method = UnityWebRequest.kHttpVerbPOST;
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Error uploading score: " + www.error);
+            }
+            else
+            {
+                Debug.Log("Score uploaded successfully!");
+            }
+        }
+    }
+}
