ead15c7c94a6d9a5384e393afb3dbc4db4961f02
jacksiayh <s10257192@connect.np.edu.sg>
2025-08-04 18:09:54 +0800

    Freeze all cars while b4 green light

--- Assets/Scripts/RaceStartLight.cs
+++ Assets/Scripts/RaceStartLight.cs
@@ -7,38 +7,88 @@ public class RaceStartLight : MonoBehaviour
     public Material RedLight;
     public Material YellowLight;
     public Material GreenLight;
-    //public Material NoLight;
 
     public float switchDuration = 1f;
-
     private Renderer StartLightRenderer;
 
-    // Start is called before the first frame update
+    private PlayersCarController[] allCars;
+
     void Start()
     {
         StartLightRenderer = GetComponent<Renderer>();
+
+        // Find all active cars in the scene at Start
+        allCars = FindObjectsOfType<PlayersCarController>();
+
         StartCoroutine(ChangeLightTexture());
     }
 
     private IEnumerator ChangeLightTexture()
     {
-        while (true)
+        // Freeze all cars
+        foreach (var car in allCars)
         {
-            StartLightRenderer.material = RedLight;
-            Debug.Log("Red Light");
-            yield return new WaitForSeconds(switchDuration);
+            car.isFrozen = true;
+        }
 
-            StartLightRenderer.material = YellowLight;
-            Debug.Log("yellow Light");
-            yield return new WaitForSeconds(switchDuration);
+        StartLightRenderer.material = RedLight;
+        Debug.Log("Red Light");
+        yield return new WaitForSeconds(switchDuration);
 
-            StartLightRenderer.material = GreenLight;
-            Debug.Log("Green Light");
-            yield return new WaitForSeconds(switchDuration);
+        StartLightRenderer.material = YellowLight;
+        Debug.Log("Yellow Light");
+        yield return new WaitForSeconds(switchDuration);
 
-            /*StartLightRenderer.material = NoLight;
-            Debug.Log("Red Light");
-            yield return new WaitForSeconds(switchDuration);*/
+        StartLightRenderer.material = GreenLight;
+        Debug.Log("Green Light");
+        yield return new WaitForSeconds(switchDuration);
+
+        // Unfreeze all cars
+        foreach (var car in allCars)
+        {
+            car.isFrozen = false;
         }
     }
 }
+
+//KaiKoon's code
+//{
+//    public Material RedLight;
+//    public Material YellowLight;
+//    public Material GreenLight;
+//    //public Material NoLight;
+
+//    public float switchDuration = 1f;
+
+//    private Renderer StartLightRenderer;
+
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        StartLightRenderer = GetComponent<Renderer>();
+//        StartCoroutine(ChangeLightTexture());
+//    }
+
+//    private IEnumerator ChangeLightTexture()
+//    {
+//        while (true)
+//        {
+//            StartLightRenderer.material = RedLight;
+//            Debug.Log("Red Light");
+//            yield return new WaitForSeconds(switchDuration);
+
+//            StartLightRenderer.material = YellowLight;
+//            Debug.Log("yellow Light");
+//            yield return new WaitForSeconds(switchDuration);
+
+//            StartLightRenderer.material = GreenLight;
+//            Debug.Log("Green Light");
+//            yield return new WaitForSeconds(switchDuration);
+
+//            /*StartLightRenderer.material = NoLight;
+//            Debug.Log("Red Light");
+//            yield return new WaitForSeconds(switchDuration);*/
+//        }
+//    }
+//}
