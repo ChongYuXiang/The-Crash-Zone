531006534adbefa587d609138f952f2edd85c013
ChongYuXiang <s10257151@connect.np.edu.sg>
2025-08-11 04:24:29 +0800

    Main Menu Update
    -Fixed audio options not working after switching scenes
    -Added mastermixer to the background

--- Assets/Scripts/VolumeDisplay.cs
+++ Assets/Scripts/VolumeDisplay.cs
@@ -2,13 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeDisplay : MonoBehaviour
 {
+    public Slider volumeSlider;
     public TextMeshProUGUI displayText;
+    public bool isBGM;
 
-    public void DisplayVolume(float value)
+    private void Update()
     {
-        displayText.text = Mathf.Round(value * 100).ToString() + "%";
+        if (isBGM)
+        {
+            displayText.text = Mathf.Round(AudioManager.instance.BGMSource.volume * 100).ToString() + "%";
+        }
+        else
+        {
+            displayText.text = Mathf.Round(AudioManager.instance.SFXSource.volume * 100).ToString() + "%";
+        }
+    }
+
+    private void Start()
+    {
+        if (isBGM)
+        {
+            displayText.text = Mathf.Round(AudioManager.instance.BGMSource.volume * 100).ToString() + "%";
+            volumeSlider.value = Mathf.Round(AudioManager.instance.BGMSource.volume*10)/10;
+        }
+        else
+        {
+            displayText.text = Mathf.Round(AudioManager.instance.SFXSource.volume * 100).ToString() + "%";
+            volumeSlider.value = Mathf.Round(AudioManager.instance.SFXSource.volume*10)/10;
+        }
     }
 }
